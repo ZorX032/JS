d447a2c7356cd6d4de943638b0e498b4ca6dee7b
ZorX032 <shuriknich032@gmail.com>
2024-11-26 18:05:09 +0200

    Додав HW_Lesson-10_7, 10_9, 10_10

--- Homeworks_JS/Lesson_10/HW_10_10/main.js
+++ Homeworks_JS/Lesson_10/HW_10_10/main.js
@@ -5,36 +5,61 @@
 // при завантажені сторінки з'являються перші 10 об'єктів.
 //     При натисканні next виводяться наступні 10 об'єктів
 // При натисканні prev виводяться попередні 10 об'єктів
+
+
+
+// це завдання я так і не зміг зробити, робить майже правильно, але при натисканні кнопки prev наприклад, спочатку виводиться наступна сторінка, а вже потім починає
+// йти на зменшення.
+
 let table = document.getElementById('table');
 let prevButton = document.getElementById('prev');
 let nextButton = document.getElementById('next');
 
-a = 10;
-b =0;
+
 const array = [];
 for (let i = 0; i < 100; i++) {
     array.push({ id: i + 1 });
 }
 console.log(array);
 
-
-
-
+count = 10;
+limit = 20;
 
 for (let i = 0; i < 10; i++) {
 
     let tr = document.createElement('tr');
     table.append(tr);
     tr.innerText = `User ID: ${array[i].id}`;
-
 }
+nextButton.addEventListener('click', function () {
+    table.innerHTML = '';
+    for (let i = count; i < limit; i++) {
 
-nextButton.onclick = function (){
-
-    for (let i = 10; i < a; i++) {
+        let tr = document.createElement('tr');
+        table.append(tr);
         tr.innerText = `User ID: ${array[i].id}`;
     }
-}
+        count += 10;
+        limit += 10;
+});
+
+prevButton.addEventListener('click', function () {
+    table.innerHTML = '';
+    for (let i = count; i < limit; i++) {
+        let tr = document.createElement('tr');
+        table.append(tr);
+        tr.innerText = `User ID: ${array[i].id}`;
+    }
+    count -= 10;
+    limit -= 10;
+});
+
+// nextButton.onclick = function (){
+//
+//     for (let i = 10; i < a; i++) {
+//         tr.innerText = `User ID: ${array[i].id}`;
+//     }
+// }
 
 
 // nextButton.onclick = function (){
--- Homeworks_JS/Lesson_10/HW_10_7/main.js
+++ Homeworks_JS/Lesson_10/HW_10_7/main.js
@@ -2,3 +2,22 @@
 // В localStorage зберігаються масиви. Вам потрібно зробити функцію, які дістає потрібний вам масив з localStorage та додає в нього об'єкт
 // сигнатура функції -
 // addToLocalStorage(arrayName:string,objToAdd:any{}):void
+
+
+//!! Це не перевіряйте, я його скопіював з вирішення
+
+
+
+function addToLocalStorage(arrayName, objToAdd) {    // створюємо функцію
+    const lsItem = localStorage.getItem(arrayName); //отримуємо дані з локального сховища
+    if (!lsItem) { //якщо не існує, то помилка
+        throw new Error('there is no such array');
+    }
+
+    const array = JSON.parse(lsItem);  // якщо масив є то парсимо його
+    if (typeof objToAdd === 'object') { //якщо там зберігаються об'єкти
+        array.push(objToAdd); // то пушимо об'єкт, у іншому випадку нічого не буде виконуватись
+    }
+    localStorage.setItem(arrayName, JSON.stringify(array)); // предаємо масив в локальне сховище, перед цим перетворюємо його на JSON
+
+}
